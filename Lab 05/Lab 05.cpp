b59c2ab0178c5753501ff13583676b8c4c0577bf
Connor Thorpe <connorthorpedo1@gmail.com>
2026-09-16 14:23:11 -0400

    Added character encoding logic

--- Lab 05/Lab 05.cpp
+++ Lab 05/Lab 05.cpp
@@ -3,9 +3,18 @@
 #include <vector>
 using namespace std;
 
-char encodeChar(char C, vector<char> cipher) {
-    // We encode the char here and output the encoded char
-    return 0;
+char encodeChar(char letter, vector<char> cipher) {
+    // We encode each letter here and output the encoded char
+
+    if (letter >= 65 && letter <= 90) { // If letter is A-Z
+        return cipher[letter - 65]; // Return position in the cipher (A = 65-65 = 0 = V)
+    }
+    else if (letter >= 97 && letter <= 122) { // If letter is a-z get the uppercase cipher and covert to lowercase
+        char upper = letter - 32;
+        upper = cipher[upper - 65];
+        return upper + 32;
+    }
+    return letter; // Returns unique characters (?, spaces, *)
 }
 
 int main() {
@@ -17,12 +26,10 @@ int main() {
     cout << "Enter text to encode: ";
     getline(cin, text);
 
-    cout << "Output: " << text;
-
     // Encode the string one character at a time
     for (int C : text) {
         encodedText += encodeChar(C, code);
     }
 
-    cout << encodedText << endl;
+    cout << "Output: " << encodedText << endl;
 }
