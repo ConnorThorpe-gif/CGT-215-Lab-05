58d9a13676a8131301e8fc54fb2dc20890e72408
Connor Thorpe <connorthorpedo1@gmail.com>
2026-09-16 14:10:12 -0400

    Added basic text input and encodeChar call

--- Lab 05/Lab 05.cpp
+++ Lab 05/Lab 05.cpp
@@ -1,20 +1,28 @@
-// Lab 05.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+char encodeChar(char C, vector<char> cipher) {
+    // We encode the char here and output the encoded char
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    vector<char> code = { 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    string text;
+    string encodedText;
+
+    cout << "Enter text to encode: ";
+    getline(cin, text);
+
+    cout << "Output: " << text;
+
+    // Encode the string one character at a time
+    for (int C : text) {
+        encodedText += encodeChar(C, code);
+    }
+
+    cout << encodedText << endl;
+}
